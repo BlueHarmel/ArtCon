46e903355dc28c5268534b39b7ef6041483f7602
이영석 <42295097+BlueHarmel@users.noreply.github.com>
2023-04-12 21:40:25 +0900

    크롤러 x,y좌표 수정

--- ArtCon/mainpage_app/urls.py
+++ ArtCon/mainpage_app/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 # url패턴에서 'mainpage_app/'이 항상 먼저 나옴
-urlpatterns = [path("", views.index, name="index")]  # /mainpage_app --> Project urls.py
+urlpatterns = [
+    path("search/", views.search_view),
+    path("map/", views.map_view),
+]  # /mainpage_app --> Project urls.py
