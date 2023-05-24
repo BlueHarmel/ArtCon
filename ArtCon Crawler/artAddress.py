bee4dd5dafd9d67198c9d6363f63672e0cf0e5b5
Young Suk Lee <42295097+BlueHarmel@users.noreply.github.com>
2023-05-24 17:50:54 +0900

    회원가입 / 비번찾기 / 전시회 정보 페이지

--- ArtCon Crawler/artAddress.py
+++ ArtCon Crawler/artAddress.py
@@ -1,8 +1,8 @@
 chromedriverAddress = "C:/Users/home/Desktop/ArtCon"
 URL = [
     "https://culture.seoul.go.kr/culture/culture/cultureEvent/list.do?searchCate=EXHIBITION&menuNo=200009",
-    "https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=bjBC&qvt=0&query=%EC%A0%84%EC%8B%9C%ED%9A%8C",
-    "http://ticket.interpark.com/TiKi/Special/TPRegionReserve.asp?ImgYn=Y&Ca=&Region=42001&RegionName=%BC%AD%BF%EF",
+    "https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=bjBC&qvt=0&query=%EC%A0%84%EC%8B%9C%ED%9A%8C", # 안됨
+    "http://ticket.interpark.com/TiKi/Special/TPRegionReserve.asp?ImgYn=Y&Ca=&Region=42001&RegionName=%BC%AD%BF%EF", # 안됨
     "http://ticket.yes24.com/New/Recommend/Area.aspx",
     "https://www.mmca.go.kr/exhibitions/progressList.do",
 ]
