de6ef4d90a37b9a2601c01f888b72056b1bf18f9
이영석 <42295097+BlueHarmel@users.noreply.github.com>
2023-05-29 02:40:05 +0900

    DB데이터 호출 간소화

--- ArtCon/mainpage_app/views.py
+++ ArtCon/mainpage_app/views.py
@@ -5,16 +5,45 @@ import json
 from exhibpage_app.models import Exhibit
 from django.core import serializers
 
+
 # Create your views here.
 # 메인페이지인데 변수 이름을 recommend라 했는데 수정 필요할지도
 def mainpage_app(request):
-    recommend1 = serializers.serialize('json', Exhibit.objects.filter(E_name__contains='꼴값쇼:'))
-    recommend2 = serializers.serialize('json', Exhibit.objects.filter(E_name__contains='파랑새를'))
-    recommend3 = serializers.serialize('json', Exhibit.objects.filter(E_name__contains='전통공예명품전'))
-    recommend4 = serializers.serialize('json', Exhibit.objects.filter(E_name__contains='달마가'))
-    banner1 = serializers.serialize('json', Exhibit.objects.filter(E_name__contains='바당수업'))
-    banner2 = serializers.serialize('json', Exhibit.objects.filter(E_name__contains='나전장의'))
-    banner3 = serializers.serialize('json', Exhibit.objects.filter(E_name__contains='오프그리드'))
-    context = {"recommend1":recommend1, "recommend2":recommend2, "recommend3":recommend3, "recommend4":recommend4,
-               "banner1":banner1, "banner2":banner2, "banner3":banner3}
-    return render(request, "mainpage_app/index.html", context=context)
+    recommend1 = Exhibit.objects.filter(E_name__contains="꼴값쇼:").values()
+    recommend2 = Exhibit.objects.filter(E_name__contains="파랑새를").values()
+    recommend3 = Exhibit.objects.filter(E_name__contains="전통공예명품전").values()
+    recommend4 = Exhibit.objects.filter(E_name__contains="달마가").values()
+    banner1 = Exhibit.objects.filter(E_name__contains="바당수업").values()
+    banner2 = Exhibit.objects.filter(E_name__contains="나전장의").values()
+    banner3 = Exhibit.objects.filter(E_name__contains="오프그리드").values()
+    # recommend1 = serializers.serialize(
+    #    "json", Exhibit.objects.filter(E_name__contains="꼴값쇼:")
+    # )
+    # recommend2 = serializers.serialize(
+    #    "json", Exhibit.objects.filter(E_name__contains="파랑새를")
+    # )
+    # recommend3 = serializers.serialize(
+    #    "json", Exhibit.objects.filter(E_name__contains="전통공예명품전")
+    # )
+    # recommend4 = serializers.serialize(
+    #    "json", Exhibit.objects.filter(E_name__contains="달마가")
+    # )
+    # banner1 = serializers.serialize(
+    #    "json", Exhibit.objects.filter(E_name__contains="바당수업")
+    # )
+    # banner2 = serializers.serialize(
+    #    "json", Exhibit.objects.filter(E_name__contains="나전장의")
+    # )
+    # banner3 = serializers.serialize(
+    #    "json", Exhibit.objects.filter(E_name__contains="오프그리드")
+    # )
+    context = {
+        "recommend1": recommend1,
+        "recommend2": recommend2,
+        "recommend3": recommend3,
+        "recommend4": recommend4,
+        "banner1": banner1,
+        "banner2": banner2,
+        "banner3": banner3,
+    }
+    return render(request, "mainpage_app/index.html", context)
