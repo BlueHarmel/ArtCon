21297fc403cdf9c91be3e9b036740b13f87d2e0a
이영석 <42295097+BlueHarmel@users.noreply.github.com>
2023-05-31 04:01:19 +0900

    추천시스템 구현

--- ArtCon Crawler/artAddress.py
+++ ArtCon Crawler/artAddress.py
@@ -1,15 +1,15 @@
 chromedriverAddress = "C:/Users/home/Desktop/ArtCon"
 URL = [
     "https://culture.seoul.go.kr/culture/culture/cultureEvent/list.do?searchCate=EXHIBITION&menuNo=200009",
-    "https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=bjBC&qvt=0&query=%EC%A0%84%EC%8B%9C%ED%9A%8C", # 안됨
-    "http://ticket.interpark.com/TiKi/Special/TPRegionReserve.asp?ImgYn=Y&Ca=&Region=42001&RegionName=%BC%AD%BF%EF", # 안됨
+    "https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=bjBC&qvt=0&query=%EC%A0%84%EC%8B%9C%ED%9A%8C",  # 안됨
+    "http://ticket.interpark.com/TiKi/Special/TPRegionReserve.asp?ImgYn=Y&Ca=&Region=42001&RegionName=%BC%AD%BF%EF",  # 안됨
     "http://ticket.yes24.com/New/Recommend/Area.aspx",
     "https://www.mmca.go.kr/exhibitions/progressList.do",
 ]
 DB = [
     "ArtCon Crawler/Crawl_Data/seouldata.json",
-    "ArtCon Crawler/Crawl_Data/naverdata.json",
-    "ArtCon Crawler/Crawl_Data/interpdata.json",
+    # "ArtCon Crawler/Crawl_Data/naverdata.json",
+    # "ArtCon Crawler/Crawl_Data/interpdata.json",
     "ArtCon Crawler/Crawl_Data/yesdata.json",
     "ArtCon Crawler/Crawl_Data/mmcadata.json",
 ]
