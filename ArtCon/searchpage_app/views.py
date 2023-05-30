dc30aae4cac682a5eb038330f4014fc7de363d2a
Jeongyoon Jang <jjy142857@gmail.com>
2023-05-31 02:02:47 +0900

    search(배치 수정 매우매우 필요할지도)

--- ArtCon/searchpage_app/views.py
+++ ArtCon/searchpage_app/views.py
@@ -3,11 +3,9 @@ from exhibpage_app.models import Exhibit
 import datetime
 
 # Create your views here.
-def search(request):
-    date = request.GET.get('date')
-    searched = list(Exhibit.objects.filter(start_date__lte=datetime.date(2023, 5, 29),
-                                        end_date__gte=datetime.date(2023,6,30)).values())
-    # for i in range(len(searched)):
-        # context[i] = searched[0]
-    context={'searched':searched}
+def search_date(request):
+    searched_date = request.POST.get('date')
+    exhibits_date = list(Exhibit.objects.filter(start_date__lte=datetime.date(2023,5,31),
+                                        end_date__gte=datetime.date(2023,5,31)).values())
+    context={'searched_date':searched_date, "exhibits": exhibits_date}
     return render(request, "searchpage_app/search.html", context=context)
