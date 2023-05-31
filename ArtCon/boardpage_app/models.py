aac2a5a02bae5dcbd43b15efc9cf06812ab587b9
Jeongyoon Jang <jjy142857@gmail.com>
2023-05-31 23:04:01 +0900

    boardpage_app model2

--- ArtCon/boardpage_app/models.py
+++ ArtCon/boardpage_app/models.py
@@ -19,3 +19,8 @@ class Comment(models.Model):
     contents = models.CharField(max_length=100)
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(datetime.datetime.now())
+
+    def __str__(self):
+        return str({'contents' : self.contents,
+                    'username' : self.username,
+                    'date' : self.date})
